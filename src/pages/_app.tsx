2bcda94711a7b14aa82c05d85ae7698cd47c4d8e
Darrell van Swinderen <darrell@wooting.io>
2022-12-09 10:49:20 +0100

    PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import ProcessesProvider from "../context/Processes";
 import { extendTheme } from "@chakra-ui/react";
 import DockProvider from "../context/DockProvider";
 import Welcome from "../components/Welcome";
+import Head from "next/head";
 
 const theme = extendTheme({
   initialColorMode: "dark",
@@ -26,16 +27,42 @@ const theme = extendTheme({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <DockProvider>
-        <ProcessesProvider>
-          <WeatherContextProvider>
-            {/* <Welcome> */}
-            <Component {...pageProps} />
-            {/* </Welcome> */}
-          </WeatherContextProvider>
-        </ProcessesProvider>
-      </DockProvider>
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icons/wooting.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/wooting.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <DockProvider>
+          <ProcessesProvider>
+            <WeatherContextProvider>
+              {/* <Welcome> */}
+              <Component {...pageProps} />
+              {/* </Welcome> */}
+            </WeatherContextProvider>
+          </ProcessesProvider>
+        </DockProvider>
+      </ChakraProvider>
+    </>
   );
 }
